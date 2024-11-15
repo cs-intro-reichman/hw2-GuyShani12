11d6e72c29190d4e570009536d421c242f5ace0b
Guy Shani <guy.shani@post.uni.ac.il>
2024-11-15 10:51:11 +0200

    first upload

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,25 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String word = args[0];
+                int n = Integer.parseInt(args[1]);
+                int count = 0;
+
+                while (count < word.length())
+                {
+                        if (args[0].charAt(count) == 'O' || args[0].charAt(count) == 'U' || args[0].charAt(count) == 'A' || args[0].charAt(count) == 'E' || args[0].charAt(count) == 'I' ){
+                        System.out.println("Give Me an " + args[0].charAt(count) + ": " + args[0].charAt(count) + "!");
+                        }
+                        else {
+                                System.out.println("Give Me a  " + args[0].charAt(count) + ": " + args[0].charAt(count) + "!");
+                        }
+                        
+                        count++;
+                }
+
+                for (int i = 0; i < n; i++){
+                        System.out.println(word + "!!!");
+                }
+
         }
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,35 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int seed = Integer.parseInt(args[0]);
+		String s = args[1];
+
+		for (int i = 1; i <= seed; i++)
+		{
+            int count =1;
+			int num = i;
+			boolean b = false;
+
+			if (s.equals("V")){
+				System.out.print(i + " ");
+			}
+			while (b == false)
+			{
+				if (num % 2 == 0){
+					num = num / 2;
+					System.out.print(num + " ");
+				}
+				else {
+					num = num * 3 + 1;
+					System.out.print(num + " ");
+				}
+				if (num == 1){
+					b = true;
+				}
+				count++;
+			}
+			System.out.println("(" + count + ")");
+		}
+		System.out.print("Every one of the first " + seed + " hailstone sequences reached 1.");
 	}
 }
