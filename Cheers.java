581c34cb8808ac206f981dc0e7b5e8a408c1d5d8
Guy Shani <guy.shani@post.uni.ac.il>
2024-11-18 09:31:49 +0200

    final upload

--- Cheers.java
+++ Cheers.java
@@ -4,12 +4,13 @@ public class Cheers {
                 String word = args[0];
                 int n = Integer.parseInt(args[1]);
                 int count = 0;
+                word = word.toUpperCase();
 
                 while (count < word.length())
                 {
                         if (args[0].charAt(count) == 'O' || args[0].charAt(count) == 'U' || args[0].charAt(count) == 'A' || args[0].charAt(count) == 'E' 
-                        || args[0].charAt(count) == 'I' || args[0].charAt(count) == 'o' || args[0].charAt(count) == 'u' || args[0].charAt(count) == 'a' 
-                        || args[0].charAt(count) == 'e' || args[0].charAt(count) == 'i' ){
+                        || args[0].charAt(count) == 'I' || args[0].charAt(count) == 'F' || args[0].charAt(count) == 'X' || args[0].charAt(count) == 'H' 
+                        || args[0].charAt(count) == 'L' || args[0].charAt(count) == 'M' || args[0].charAt(count) == 'N'|| args[0].charAt(count) == 'R'){
                         System.out.println("Give Me an " + args[0].charAt(count) + ": " + args[0].charAt(count) + "!");
                         }
                         else {
